73f02a572cdd1f6e6c76751eb24ef943f855a931
Alvaro <alvaro.alvaro601@gmail.com>
2026-03-03 23:12:29 +0100

    feat: agregando archivos al modulo dto incluyendo LoginRequest.java, AuthSessionResponse.java, ResetPasswordRequest.java

---
+++ backend/src/main/java/com/alvar/oasisclub/auth/dto/AuthSessionResponse.java
@@ -0,0 +1,14 @@
+package com.alvar.oasisclub.auth.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthSessionResponse {
+  private String token;
+  private String clientId;
+  private String name;
+  private String email;
+  private String role;
+  private String plan;
+}
+
