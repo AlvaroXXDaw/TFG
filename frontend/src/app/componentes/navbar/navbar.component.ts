f36639d0695429ba180b5ed6b3feea746c03597a
Emilio <emiliobeniloza@gmail.com>
2026-03-18 23:26:05 +0100

    feat: creacion de navbar.component.ts

---
+++ frontend/src/app/componentes/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthStore } from '../../auth.store';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {
+  auth = inject(AuthStore);
+  private router = inject(Router);
+  isMobileMenuOpen = signal(false);
+
+  logout() {
+    this.auth.logout();
+    this.isMobileMenuOpen.set(false);
+    this.router.navigate(['/']);
+  }
+}
