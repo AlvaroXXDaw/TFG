fdc806c7117a915feac8e16106839bc247b16e60
AlvaroXXDaw <alvaro.alvaro601@gmail.com>
2026-04-15 20:39:27 +0200

    admin

--- backend/src/main/resources/db/migration/V7__update_admin_password.sql
+++ backend/src/main/resources/db/migration/V7__update_admin_password.sql
@@ -1,4 +1,4 @@
 UPDATE clients 
 SET email = 'alvaro@admin.com',
-    password_hash = '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgCcU7uxpJWPUUnVBeasH5xc4JZ.' 
+    password_hash = '$2a$10$48yJIxScSScEXagEc5.hoOXJV8vPyoIqeEnlesgrE05K/LnY5EHWW' 
 WHERE role = 'ADMIN';
